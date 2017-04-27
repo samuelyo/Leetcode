03ba632435b3bb6db56b63290609d97868131759
samuelyo <1120780416@qq.com>
2017-04-27 21:11:10 +0800

    Leetcode 283

---
+++ 283MoveZeroes2.java
@@ -0,0 +1,30 @@
+package com.leetcode.movezeroes;
+
+import java.util.Arrays;
+
+public class MoveZeroes2 {
+
+	public static void main(String[] args) {
+		
+		int[] nums = {0,0,0,3,12};
+		moveZeroes(nums);
+		System.out.println(Arrays.toString(nums));
+
+	}
+
+	public static void moveZeroes(int[] nums){
+		
+		if (nums == null || nums.length == 0) return;        
+
+	    int insertPos = 0;
+	    for (int num: nums) {
+	        if (num != 0) nums[insertPos++] = num;
+	    }        
+
+	    while (insertPos < nums.length) {
+	        nums[insertPos++] = 0;
+	    }
+	    
+	}
+	
+}
