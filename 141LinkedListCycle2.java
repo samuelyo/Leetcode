02078360dedebb01401daceb03d0f99d9431ec07
samuelyo <1120780416@qq.com>
2017-05-06 14:26:09 +0800

    Leetcode 141

---
+++ 141LinkedListCycle2.java
@@ -0,0 +1,19 @@
+package com.leetcode.linkedlistcycle;
+
+public class LinkedListCycle2 {
+
+	public static boolean hasCycle(ListNode head){
+	
+		if(head==null){
+			return false;
+		}
+	    ListNode walker = head;
+	    ListNode runner = head;
+	    while(runner.next!=null && runner.next.next!=null) {
+	        walker = walker.next;
+	        runner = runner.next.next;
+	        if(walker==runner) return true;
+	    }
+	    return false;
+	}
+}
