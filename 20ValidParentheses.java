346fbdbe5809349d5792b4c8775bf31418ff609b
samuelyo <1120780416@qq.com>
2017-05-18 15:08:06 +0800

    Leetcode 20 only ] } )

---
+++ 20ValidParentheses.java
@@ -0,0 +1,31 @@
+package com.leetcode.validparentheses;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public static void main(String[] args) {
+		
+		String s = "[[]]";
+		System.out.println(isValid(s));
+
+	}
+	
+	public static boolean isValid(String s){
+		
+		Stack<Character> stack = new Stack<Character>();
+		for (char c : s.toCharArray()) {
+			if (c == '(')
+				stack.push(')');
+			else if (c == '{')
+				stack.push('}');
+			else if (c == '[')
+				stack.push(']');
+			else if (stack.isEmpty() || stack.pop() != c)
+				return false;
+		}
+		return stack.isEmpty();
+		
+	}
+
+}
