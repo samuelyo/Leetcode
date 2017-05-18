e0bfc0eacbe995e3374d33a20e7e4f0499149063
samuelyo <1120780416@qq.com>
2017-05-18 15:02:51 +0800

    Leetcode 14 indexOf

---
+++ 14LongestCommonPrefix2.java
@@ -0,0 +1,24 @@
+package com.leetcode.longestcommonprefix;
+
+public class LongestCommonPrefix2 {
+
+	public static void main(String[] args) {
+
+		String[] strs = { "abc", "abcd","ab" };
+		System.out.println(longestCommonPrefix(strs));
+
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+
+		if (strs.length == 0)
+			return "";
+		String pre = strs[0];
+		for (int i = 1; i < strs.length; i++)
+			while (strs[i].indexOf(pre) != 0)
+				pre = pre.substring(0, pre.length() - 1);
+		return pre;
+
+	}
+
+}
