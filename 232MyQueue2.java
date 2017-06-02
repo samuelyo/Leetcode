0ecff7677df1859e852067b1508b67b31733cc98
samuelyo <1120780416@qq.com>
2017-06-02 18:29:34 +0800

    Leetcode 232

---
+++ 232MyQueue2.java
@@ -0,0 +1,30 @@
+package com.leetcode.ImplementQueueUsingStacks;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class MyQueue2 {
+
+	Stack<Integer> input = new Stack();
+	Stack<Integer> output = new Stack();
+
+	public void push(int x) {
+		input.push(x);
+	}
+
+	public int pop() {
+		peek();
+		return output.pop();
+	}
+
+	public int peek() {
+		if (output.empty())
+			while (!input.empty())
+				output.push(input.pop());
+		return output.peek();
+	}
+
+	public boolean empty() {
+		return input.empty() && output.empty();
+	}
+}
