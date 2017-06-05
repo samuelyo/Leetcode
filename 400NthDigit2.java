85636bdef819c56570666ad70c6493265a9ee378
samuelyo <1120780416@qq.com>
2017-06-05 21:46:36 +0800

    Leetcode 400

---
+++ 400NthDigit2.java
@@ -0,0 +1,38 @@
+package com.leetcode.NthDigit;
+
+public class NthDigit2 {
+
+	public static void main(String[] args) {
+
+		int n = 200;
+		System.out.println(findNthDigit(n));
+
+	}
+
+	/**
+	 * 1¡¢find the length of the number where the nth digit is from
+	 * 2¡¢find the actual number where the nth digit is from
+	 * 3¡¢find the nth digit and return
+	 * @param n
+	 * @return
+	 */
+	public static int findNthDigit(int n) {
+
+		int len = 1;
+		long count = 9;
+		int start = 1;
+
+		while (n > len * count) {
+			n -= len * count;
+			len += 1;
+			count *= 10;
+			start *= 10;
+		}
+
+		start += (n - 1) / len;
+		String s = Integer.toString(start);
+		return Character.getNumericValue(s.charAt((n - 1) % len));
+
+	}
+
+}
