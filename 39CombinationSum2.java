0552f8033110d89d66449121e044c95ab9c6e1f0
samuelyo <1120780416@qq.com>
2017-06-10 13:24:14 +0800

    Leetcode 39 Backtracking

---
+++ 39CombinationSum2.java
@@ -0,0 +1,45 @@
+package com.leetcode.CombinationSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CombinationSum2 {
+
+	public static void main(String[] args) {
+
+		int[] candidates = { 2, 3, 6, 7 };
+		int target = 7;
+		List<List<Integer>> ans = combinationSum(candidates, target);
+		for (int i = 0; i < ans.size(); i++) {
+			List<Integer> res = ans.get(i);
+			for (int j = 0; j < res.size(); j++) {
+				System.out.print(res.get(j) + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+		List<List<Integer>> list = new ArrayList<>();
+	    Arrays.sort(candidates);
+	    backtrack(list, new ArrayList<>(), candidates, target, 0);
+	    return list;
+	}
+
+	private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+	    if(remain < 0) return;
+	    else if(remain == 0) list.add(new ArrayList<>(tempList));
+	    else{ 
+	        for(int i = start; i < nums.length && nums[i] <= remain; i++){
+	            tempList.add(nums[i]);
+	            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+	            tempList.remove(tempList.size() - 1);
+	        }
+	    }
+	}
+
+}
