c093943b9b0e35e7e3a231315805ddea59398aeb
samuelyo <1120780416@qq.com>
2017-06-20 16:53:40 +0800

    Leetcode 442 nums[nums[i]

---
+++ 442FindAllDuplicatesInAnArray2.java
@@ -0,0 +1,33 @@
+package com.leetcode.FindAllDuplicatesInAnArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindAllDuplicatesInAnArray2 {
+
+	public static void main(String[] args) {
+		
+		int[] nums = {4,3,2,7,8,2,3,1};
+		List<Integer> ans = findDuplicates(nums);
+		for(int i = 0;i<ans.size();i++){
+			System.out.print(ans.get(i) + " ");
+		}
+
+	}
+
+	public static List<Integer> findDuplicates(int[] nums) {
+		
+		// when find a number i, flip the number at position i-1 to negative. 
+	    // if the number at position i-1 is already negative, i is the number that occurs twice.
+		List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(index+1);
+            nums[index] = -nums[index];
+        }
+        return res;
+	}
+}
