1701c3b7c4050355fd2d0fc00ca317b57b123176
samuelyo <1120780416@qq.com>
2017-06-27 16:40:28 +0800

    Leetcode 454 Map<C[i] + D[i],count>

---
+++ 454FourSumII2.java
@@ -0,0 +1,41 @@
+package com.leetcode.FourSumII;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumII2 {
+
+	public static void main(String[] args) {
+
+		int[] A = { 1, 2 };
+		int[] B = { -2, -1 };
+		int[] C = { -1, 2 };
+		int[] D = { 0, 2 };
+		int ans = fourSumCount(A, B, C, D);
+		System.out.println(ans);
+
+	}
+
+	public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < C.length; i++) {
+			for (int j = 0; j < D.length; j++) {
+				int sum = C[i] + D[j];
+				map.put(sum, map.getOrDefault(sum, 0) + 1);
+			}
+		}
+
+		int res = 0;
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < B.length; j++) {
+				res += map.getOrDefault(-1 * (A[i] + B[j]), 0);
+			}
+		}
+
+		return res;
+
+	}
+
+}
