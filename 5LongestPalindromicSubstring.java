c27dfb2525ae339d20f573192228a22e227ce194
samuelyo <1120780416@qq.com>
2017-06-27 15:48:41 +0800

    Leetcode 5 consider the center is odd or even length to extend as long as possible

---
+++ 5LongestPalindromicSubstring.java
@@ -0,0 +1,42 @@
+package com.leetcode.LongestPalindromicSubstring;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LongestPalindromicSubstring {
+
+	public static void main(String[] args) {
+
+		String s = "babad";
+		System.out.println(longestPalindrome(s));
+
+	}
+
+	private static int lo, maxLen;
+
+	public static String longestPalindrome(String s) {
+		int len = s.length();
+		if (len < 2)
+			return s;
+
+		for (int i = 0; i < len - 1; i++) {
+			extendPalindrome(s, i, i); // assume center is odd length, try to extend
+										// Palindrome as possible
+			extendPalindrome(s, i, i + 1); // assume center is even length.
+		}
+		return s.substring(lo, lo + maxLen);
+	}
+
+	private static void extendPalindrome(String s, int j, int k) {
+		while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+			j--;
+			k++;
+		}
+		if (maxLen < k - j - 1) {
+			lo = j + 1;
+			maxLen = k - j - 1;
+		}
+	}
+}
