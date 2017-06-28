7a011c892aac05956fa04e17dd988e39750f2803
samuelyo <1120780416@qq.com>
2017-06-28 20:09:52 +0800

    Leetcode 537

---
+++ 537ComplexNumberMultiplication3.java
@@ -0,0 +1,35 @@
+package com.leetcode.ComplexNumberMultiplication;
+
+public class ComplexNumberMultiplication3 {
+
+	public static void main(String[] args) {
+
+		String a = "1+-1i";
+		String b = "1+-1i";
+		System.out.println(complexNumberMultiply(a, b));
+
+	}
+
+	public static String complexNumberMultiply(String a, String b) {
+
+		int[] valA = getValue(a);
+		int[] valB = getValue(b);
+
+		int real = valA[0] * valB[0] - valA[1] * valB[1];
+		int img = valA[0] * valB[1] + valA[1] * valB[0];
+
+		return real + "+" + img + "i";
+	}
+
+	private static int[] getValue(String s) {
+		String[] str = s.split("\\+");
+		int[] val = new int[2];
+		val[0] = Integer.valueOf(str[0]);
+		int indexOfI = str[1].indexOf("i");
+		val[1] = Integer.valueOf(str[1].substring(0, indexOfI));
+
+		return val;
+
+	}
+
+}
