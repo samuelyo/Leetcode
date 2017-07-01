c69a4606488e9a93e7a6cd6a6d92a892f906365f
samuelyo <1120780416@qq.com>
2017-07-01 21:10:10 +0800

    Leetcode 522 Sort the Array by length and loop the array to check if it is the subsequence of others

---
+++ 522LongestUncommonSubsequenceII2.java
@@ -0,0 +1,55 @@
+package com.leetcode.LongestUncommonSubsequenceII;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class LongestUncommonSubsequenceII2 {
+
+	public static void main(String[] args) {
+
+		String[] strs = { "aaa", "aaa", "a"};
+		System.out.println(findLUSlength(strs));
+
+	}
+
+	public static int findLUSlength(String[] strs) {
+
+		int len = strs.length;
+
+		// reverse sorting array with length
+		Arrays.sort(strs, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				return s2.length() - s1.length();
+			}
+		});
+
+		for (int i = 0; i < len; i++) {
+			int missMatchCount = strs.length - 1;
+			for (int j = 0; j < len; j++) {
+				if (i != j && !isSubSequence(strs[i], strs[j])) {
+					missMatchCount--;
+				}
+			}
+
+			// strs[i] is not a sub sequence of any other entry
+			if (missMatchCount == 0) {
+				return strs[i].length();
+			}
+		}
+
+		return -1;
+	}
+
+	public static boolean isSubSequence(String s1, String s2) {
+		int idx = 0;
+		for (char ch : s2.toCharArray()) {
+			if (idx < s1.length() && ch == s1.charAt(idx)) {
+				idx++;
+			}
+		}
+
+		return idx == s1.length();
+	}
+
+}
