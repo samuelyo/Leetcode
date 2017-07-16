fe250dada2cc364162f9ec473245915ec7b28c1e
samuelyo <1120780416@qq.com>
2017-07-16 16:45:42 +0800

    Leetcode 365 another way to compute the greatest common divisor

---
+++ 365WaterAndJugProblem3.java
@@ -0,0 +1,60 @@
+package com.leetcode.WaterAndJugProblem;
+
+public class WaterAndJugProblem3 {
+
+	public static void main(String[] args) {
+
+		int x = 6;
+		int y = 9;
+		int z = 1;
+		System.out.println(canMeasureWater(x, y, z));
+
+	}
+
+	/**
+	 * B¨¦zout's identity (also called B¨¦zout's lemma) is a theorem in the
+	 * elementary theory of numbers:
+	 * 
+	 * let a and b be nonzero integers and let d be their greatest common
+	 * divisor. Then there exist integers x and y such that ax+by=d
+	 * 
+	 * In addition, the greatest common divisor d is the smallest positive
+	 * integer that can be written as ax + by
+	 * 
+	 * every integer of the form ax + by is a multiple of the greatest common
+	 * divisor d.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @return
+	 */
+	public static boolean canMeasureWater(int x, int y, int z) {
+
+		// limit brought by the statement that water is finallly in one or both
+		// buckets
+		if (x + y < z){
+			return false;
+		}
+	
+		// case x or y is zero
+		if (x == z || y == z || x + y == z){
+			return true;
+		}
+			
+		// get GCD, then we can use the property of B¨¦zout's identity
+		return z % GCD(x, y) == 0;
+	}
+
+	public static int GCD(int a, int b) {
+		while (true) {
+			if ((a = a % b) == 0){
+				return b;
+			}
+			if ((b = b % a) == 0){
+				return a;
+			}
+		}
+	}
+
+}
