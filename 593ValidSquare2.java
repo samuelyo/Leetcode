0170f8c846f79c3ff495c77a5d7b1890faf9739b
samuelyo <1120780416@qq.com>
2017-07-19 15:03:15 +0800

    Leetcode 593 check six lengths whether has two longer sides and four equeal sides

---
+++ 593ValidSquare2.java
@@ -0,0 +1,48 @@
+package com.leetcode.ValidSquare;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidSquare2 {
+
+	public static void main(String[] args) {
+
+		int[] p1 = { 0, 0 };
+		int[] p2 = { 1, 1 };
+		int[] p3 = { 1, 0 };
+		int[] p4 = { 0, 1 };
+		System.out.println(validSquare(p1, p2, p3, p4));
+
+	}
+
+	public static boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+
+		long[] lengths = { length(p1, p2), length(p2, p3), length(p3, p4), length(p4, p1), length(p1, p3),
+				length(p2, p4) }; // all 6 sides
+
+		long max = 0, nonMax = 0;
+		for (long len : lengths) {
+			max = Math.max(max, len);
+		}
+		int count = 0;
+		for (int i = 0; i < lengths.length; i++) {
+			if (lengths[i] == max)
+				count++;
+			else
+				nonMax = lengths[i]; // non diagonal side.
+		}
+		if (count != 2)
+			return false; // diagonals lenghts have to be same.
+
+		for (long len : lengths) {
+			if (len != max && len != nonMax)
+				return false; // sides have to be same length
+		}
+		return true;
+	}
+
+	private static long length(int[] p1, int[] p2) {
+		return (long) Math.pow(p1[0] - p2[0], 2) + (long) Math.pow(p1[1] - p2[1], 2);
+
+	}
+}
