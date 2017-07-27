569604c7d10983d6affcf8c738c8a92dadc5fe3e
samuelyo <1120780416@qq.com>
2017-07-27 12:13:25 +0800

    Leetcode 617 Recursion and use the newNode as the result node with the value of two nodes's sum

---
+++ 617MergeTwoBinaryTrees2.java
@@ -0,0 +1,49 @@
+package com.leetcode.MergeTwoBinaryTrees;
+
+public class MergeTwoBinaryTrees2 {
+
+	public static void main(String[] args) {
+
+		TreeNode t1 = new TreeNode(1);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node5 = new TreeNode(5);
+
+		t1.left = node3;
+		t1.right = node2;
+		node3.left = node5;
+
+		TreeNode t2 = new TreeNode(2);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node3r = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node7 = new TreeNode(7);
+
+		t2.left = node1;
+		t2.right = node3r;
+		node1.right = node4;
+		node3r.right = node7;
+
+		TreeNode res = mergeTrees(t1, t2);
+		System.out.println(res.val);
+		System.out.println(res.left.val);
+		System.out.println(res.right.val);
+
+	}
+
+	public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+		if (t1 == null && t2 == null)
+			return null;
+
+		int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+		TreeNode newNode = new TreeNode(val);
+
+		newNode.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+		newNode.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+
+		return newNode;
+
+	}
+
+}
