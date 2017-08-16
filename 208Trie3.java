6ff37f65b9f9def6d28edb33990f9494fb3c53ef
samuelyo <1120780416@qq.com>
2017-08-16 09:08:44 +0800

    Leetcode 208 TrieNode and boolean isWord and loop checking if trienode is null and return isWord or true always

---
+++ 208Trie3.java
@@ -0,0 +1,51 @@
+package com.leetcode.ImplementTriePrefixTree;
+
+public class Trie3 {
+	
+	class TrieNode {
+		
+		public boolean isWord;
+		public TrieNode[] children = new TrieNode[26];
+
+	}
+
+	private TrieNode root;
+
+	public Trie3() {
+        root = new TrieNode();
+    }
+
+	public void insert(String word) {
+		TrieNode ws = root;
+		for (int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			if (ws.children[c - 'a'] == null) {
+				ws.children[c - 'a'] = new TrieNode();
+			}
+			ws = ws.children[c - 'a'];
+		}
+		ws.isWord = true;
+	}
+
+	public boolean search(String word) {
+		TrieNode ws = root;
+		for (int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			if (ws.children[c - 'a'] == null)
+				return false;
+			ws = ws.children[c - 'a'];
+		}
+		return ws.isWord;
+	}
+
+	public boolean startsWith(String prefix) {
+		TrieNode ws = root;
+		for (int i = 0; i < prefix.length(); i++) {
+			char c = prefix.charAt(i);
+			if (ws.children[c - 'a'] == null)
+				return false;
+			ws = ws.children[c - 'a'];
+		}
+		return true;
+	}
+}
