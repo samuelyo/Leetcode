4c7d50e65a3a3e84f43c62c362a6682e2b0a8044
samuelyo <1120780416@qq.com>
2017-08-18 09:53:24 +0800

    Leetcode 341 Stack and firstly add all nodes into the stack looply from back to front secondly check the peek if a integer and thirdly if not just pop and add all its nodes into the stack looply from back to front

---
+++ 341NestedIterator2.java
@@ -0,0 +1,39 @@
+package com.leetcode.FlattenNestedListIterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+import com.leetcode.MiniParser.NestedInteger;
+
+public class NestedIterator2 implements Iterator<Integer> {
+
+	Stack<NestedInteger> stack = new Stack<>();
+
+	public NestedIterator2(List<NestedInteger> nestedList) {
+		for (int i = nestedList.size() - 1; i >= 0; i--) {
+			stack.push(nestedList.get(i));
+		}
+	}
+
+	@Override
+	public Integer next() {
+		return stack.pop().getInteger();
+	}
+
+	@Override
+	public boolean hasNext() {
+		while (!stack.isEmpty()) {
+			NestedInteger curr = stack.peek();
+			if (curr.isInteger()) {
+				return true;
+			}else{
+				stack.pop();
+				for (int i = curr.getList().size() - 1; i >= 0; i--) {
+					stack.push(curr.getList().get(i));
+				}
+			}
+		}
+		return false;
+	}
+}
