f5e7bab1392b5a60900a5dde166ab3da53a7e325
samuelyo <1120780416@qq.com>
2017-08-26 10:21:22 +0800

    Leetcode 393 keep the count as the different kinds such as 3 ,2 , 1 then current data >> 5 == 0b110(binary representation) and at last check if count equals 0

---
+++ 393UTF8Validation2.java
@@ -0,0 +1,35 @@
+package com.leetcode.UTF8Validation;
+
+public class UTF8Validation2 {
+
+	public static void main(String[] args) {
+
+		int[] data = { 255 };
+		System.out.println(validUtf8(data));
+
+	}
+
+	public static boolean validUtf8(int[] data) {
+
+		int count = 0;
+		for (int c : data) {
+			if (count == 0) {
+				if ((c >> 5) == 0b110)
+					count = 1;
+				else if ((c >> 4) == 0b1110)
+					count = 2;
+				else if ((c >> 3) == 0b11110)
+					count = 3;
+				else if ((c >> 7) != 0)
+					return false;
+			} else {
+				if ((c >> 6) != 0b10)
+					return false;
+				count--;
+			}
+		}
+		return count == 0;
+		
+	}
+
+}
