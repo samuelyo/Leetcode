cb53c9a946e493f7d465e6e3151b790872442d92
samuelyo <1120780416@qq.com>
2017-08-28 10:47:28 +0800

    Leetcode 241 Divide and conquer and if(list.size == 0) return input

---
+++ 241DifferentWaysToAddParentheses2.java
@@ -0,0 +1,52 @@
+package com.leetcode.DifferentWaysToAddParentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DifferentWaysToAddParentheses2 {
+
+	public static void main(String[] args) {
+
+		String input = "2*3-4*5";
+		List<Integer> res = diffWaysToCompute(input);
+		for (int i = 0; i < res.size(); i++) {
+			System.out.print(res.get(i) + " ");
+		}
+
+	}
+
+	public static List<Integer> diffWaysToCompute(String input) {
+
+		List<Integer> ret = new ArrayList<Integer>();
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == '-' || input.charAt(i) == '*' || input.charAt(i) == '+') {
+				String part1 = input.substring(0, i);
+				String part2 = input.substring(i + 1);
+				List<Integer> part1Ret = diffWaysToCompute(part1);
+				List<Integer> part2Ret = diffWaysToCompute(part2);
+				for (Integer p1 : part1Ret) {
+					for (Integer p2 : part2Ret) {
+						int c = 0;
+						switch (input.charAt(i)) {
+						case '+':
+							c = p1 + p2;
+							break;
+						case '-':
+							c = p1 - p2;
+							break;
+						case '*':
+							c = p1 * p2;
+							break;
+						}
+						ret.add(c);
+					}
+				}
+			}
+		}
+		if (ret.size() == 0) {
+			ret.add(Integer.valueOf(input));
+		}
+		return ret;
+
+	}
+}
