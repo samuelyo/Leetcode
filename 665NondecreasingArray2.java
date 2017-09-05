3bf712d6b20e93647757f2d1f8737af457356566
samuelyo <1120780416@qq.com>
2017-09-06 01:23:43 +0800

    Leetcode 665 looply find the noincreasing index and count ++ and firstly change the nums[i-1] and nums[i-2] <= nums[i] secondly change the num[i] and continue loop and at last check count <= 1

---
+++ 665NondecreasingArray2.java
@@ -0,0 +1,28 @@
+package com.leetcode.NondecreasingArray;
+
+public class NondecreasingArray2 {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 1, 3, 2 };
+		System.out.println(checkPossibility(nums));
+
+	}
+
+	public static boolean checkPossibility(int[] nums) {
+
+		int cnt = 0; // the number of changes
+		for (int i = 1; i < nums.length && cnt <= 1; i++) {
+			if (nums[i - 1] > nums[i]) {
+				cnt++;
+				if (i - 2 < 0 || nums[i - 2] <= nums[i])
+					nums[i - 1] = nums[i]; // modify nums[i-1] of a priority
+				else
+					nums[i] = nums[i - 1]; // have to modify nums[i]
+			}
+		}
+		return cnt <= 1;
+
+	}
+
+}
