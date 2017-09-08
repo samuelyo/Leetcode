451c1073194bf012e240d11ee251cb0c727a2318
samuelyo <1120780416@qq.com>
2017-09-08 09:14:48 +0800

    Leetcode 473 Firstly check if sum % 4 == 0 secondly dfs and backtracking before sort the nums from big to small then dfs(int[] nums, int[] sums, int index, int target)

---
+++ 473MatchsticksToSquare3.java
@@ -0,0 +1,60 @@
+package com.leetcode.MatchsticksToSquare;
+
+import java.util.Arrays;
+
+public class MatchsticksToSquare3 {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 5, 5, 5, 5, 16, 4, 4, 4, 4, 4, 3, 3, 3, 3, 4 };
+		System.out.println(makesquare(nums));
+	}
+
+	public static boolean makesquare(int[] nums) {
+
+		if (nums == null || nums.length < 4)
+			return false;
+		int sum = 0;
+		for (int num : nums)
+			sum += num;
+		if (sum % 4 != 0)
+			return false;
+
+		Arrays.sort(nums);
+		reverse(nums);
+
+		return dfs(nums, new int[4], 0, sum / 4);
+	}
+
+	public static boolean dfs(int[] nums, int[] sums, int index, int target) {
+		if (index == nums.length) {
+			if (sums[0] == target && sums[1] == target && sums[2] == target) {
+				return true;
+			}
+			return false;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			if (sums[i] + nums[index] > target)
+				continue;
+			sums[i] += nums[index];
+			if (dfs(nums, sums, index + 1, target))
+				return true;
+			sums[i] -= nums[index];
+		}
+
+		return false;
+	}
+
+	public static void reverse(int[] nums) {
+		int i = 0, j = nums.length - 1;
+		while (i < j) {
+			int temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
+			i++;
+			j--;
+		}
+	}
+
+}
