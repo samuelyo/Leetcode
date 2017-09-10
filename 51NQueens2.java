a06b0d3667e472dd392fdfacc8370e99ae93bc7f
samuelyo <1120780416@qq.com>
2017-09-10 10:41:49 +0800

    Leetcode 51 String[] board and char[] initial and Arrays.fill(initial,'.') and Arrays.fill(board,String.valueOf(initial)) and boolean[n] cols and boolean[2*n] d1 and boolean[2*n] d2 and helper(res,board,cols,d1,d2,0,n)

---
+++ 51NQueens2.java
@@ -0,0 +1,66 @@
+package com.leetcode.NQueens;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NQueens2 {
+
+	public static void main(String[] args) {
+		
+		int n = 4;
+		List<List<String>> res = solveNQueens(n);
+		for(int i = 0;i<res.size();i++){
+			List<String> cur = res.get(i);
+			for(int j = 0;j<cur.size();j++){
+				System.out.println(cur.get(j));
+			}
+			System.out.println();
+		}
+
+	}
+	
+	public static List<List<String>> solveNQueens(int n) {
+		
+		List<List<String>> res = new ArrayList<List<String>>();
+		String[] board = new String[n];
+		char[] initial = new char[n];
+		Arrays.fill(initial, '.');
+		Arrays.fill(board, String.valueOf(initial));
+		boolean[] cols = new boolean[n], d1 = new boolean[2 * n], d2 = new boolean[2 * n];
+		helper(res, board, cols, d1, d2, 0, n);
+		return res;
+	}
+
+	public static void helper(List<List<String>> res, String[] board, boolean[] cols, boolean[] d1, boolean[] d2,
+			int row, int n) {
+		if (row == n) {
+			res.add(new ArrayList<String>(Arrays.asList(board)));
+			return;
+		}
+
+		for (int col = 0; col < n; col++) {
+			if (cols[col] || d1[row + col] || d2[row - col + n])
+				continue;
+
+			cols[col] = true;
+			d1[row + col] = true;
+			d2[row - col + n] = true;
+			char[] cur = board[row].toCharArray();
+			cur[col] = 'Q';
+			board[row] = String.valueOf(cur);
+
+			helper(res, board, cols, d1, d2, row + 1, n);
+
+			cur = board[row].toCharArray();
+			cur[col] = '.';
+			board[row] = String.valueOf(cur);
+
+			cols[col] = false;
+			d1[row + col] = false;
+			d2[row - col + n] = false;
+		}
+		
+	}
+
+}
