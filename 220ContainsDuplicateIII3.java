43a4087663d1698abddde03f56ab2d9d566170ac
samuelyo <1120780416@qq.com>
2017-09-11 10:01:15 +0800

    Leetcode 220 Map<Long, Long> map and long remappedNum = (long) nums[i] - Integer.MIN_VALUE and long bucket = remappedNum / ((long) t + 1) and if (map.containsKey(bucket) || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)|| (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t)) return true and when map.entrySet.size >= k then long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1); and map.remove(lastBucket)

---
+++ 220ContainsDuplicateIII3.java
@@ -0,0 +1,37 @@
+package com.leetcode.ContainsDuplicateIII;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateIII3 {
+
+	public static void main(String[] args) {
+
+		int[] nums = { -2147483648, -2147483647 };
+		int k = 3;
+		int t = 3;
+		System.out.println(containsNearbyAlmostDuplicate(nums, k, t));
+
+	}
+
+	public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+		if (k < 1 || t < 0)
+			return false;
+		Map<Long, Long> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
+			long bucket = remappedNum / ((long) t + 1);
+			if (map.containsKey(bucket) || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
+					|| (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
+				return true;
+			if (map.entrySet().size() >= k) {
+				long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+				map.remove(lastBucket);
+			}
+			map.put(bucket, remappedNum);
+		}
+		return false;
+
+	}
+}
