1a9a055c1b4f1679dfa1f0c187efe4f743458681
samuelyo <1120780416@qq.com>
2017-09-13 10:34:39 +0800

    Leetcode 42 Two pointers left and right and update maxleft and maxright and res += maxleft - height[left] and res += maxright - height[right]

---
+++ 42TrappingRainWater2.java
@@ -0,0 +1,37 @@
+package com.leetcode.TrappingRainWater;
+
+public class TrappingRainWater2 {
+
+	public static void main(String[] args) {
+
+		int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+		System.out.println(trap(height));
+
+	}
+
+	public static int trap(int[] height) {
+
+		int n = height.length;
+		int left = 0;
+		int right = n - 1;
+		int res = 0;
+		int maxleft = 0, maxright = 0;
+		while (left <= right) {
+			if (height[left] <= height[right]) {
+				if (height[left] >= maxleft)
+					maxleft = height[left];
+				else
+					res += maxleft - height[left];
+				left++;
+			} else {
+				if (height[right] >= maxright)
+					maxright = height[right];
+				else
+					res += maxright - height[right];
+				right--;
+			}
+		}
+		return res;
+
+	}
+}
