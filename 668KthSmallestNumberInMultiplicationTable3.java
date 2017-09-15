181c98c52ef9c480420cecdbd0474936e33d4285
samuelyo <1120780416@qq.com>
2017-09-15 09:26:54 +0800

    Leetcode 668 Binary Search and Find number in an increasing row and col matrix and j = n and count += Math.min(n, mul / i)

---
+++ 668KthSmallestNumberInMultiplicationTable3.java
@@ -0,0 +1,41 @@
+package com.leetcode.KthSmallestNumberInMultiplicationTable;
+
+public class KthSmallestNumberInMultiplicationTable3 {
+
+	public static void main(String[] args) {
+
+		int m = 2;
+		int n = 3;
+		int k = 5;
+		System.out.println(findKthNumber(m, n, k));
+
+	}
+
+	public static int findKthNumber(int m, int n, int k) {
+
+		int low = 1;
+		int high = m * n + 1;
+		while(low < high){
+			int mid = (high - low) / 2 + low;
+			if(findKthNumberHelp(m, n, mid) < k){
+				low = mid + 1;
+			}else {
+				high = mid;
+			}
+		}
+		return low;
+
+	}
+
+	public static int findKthNumberHelp(int m, int n, int mul) {
+
+		int count = 0;
+//		int j = n;
+		for(int i = 1;i<=m;i++){
+			count += Math.min(n, mul / i);
+		}
+		return count;
+
+	}
+
+}
