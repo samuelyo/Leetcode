8ff114e9021c1ca8ab860a6136d268d71de37068
samuelyo <1120780416@qq.com>
2017-09-18 12:53:12 +0800

    Leetcode 239 Firstly use a queue to store the index secondly loop and do following things,firstly remove numbers out of range k secondly remove smaller numbers in k range as they are useless which from the queue tail which is smaller than current num,thirdly push the cur num into the queue and store the queue head num into the res

---
+++ 239SlidingWindowMaximum2.java
@@ -0,0 +1,47 @@
+package com.leetcode.SlidingWindowMaximum;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SlidingWindowMaximum2 {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
+		int k = 3;
+		int[] res = maxSlidingWindow(nums, k);
+		for (int i = 0; i < res.length; i++) {
+			System.out.print(res[i] + " ");
+		}
+
+	}
+
+	public static int[] maxSlidingWindow(int[] nums, int k) {
+
+		if (nums == null || k <= 0) {
+			return new int[0];
+		}
+		int n = nums.length;
+		int[] r = new int[n - k + 1];
+		int ri = 0;
+		// store index
+		Deque<Integer> q = new ArrayDeque<>();
+		for (int i = 0; i < nums.length; i++) {
+			// remove numbers out of range k
+			while (!q.isEmpty() && q.peek() < i - k + 1) {
+				q.poll();
+			}
+			// remove smaller numbers in k range as they are useless
+			while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+				q.pollLast();
+			}
+			// q contains index... r contains content
+			q.offer(i);
+			if (i >= k - 1) {
+				r[ri++] = nums[q.peek()];
+			}
+		}
+		return r;
+
+	}
+}
