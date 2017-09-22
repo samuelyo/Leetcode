ab6c97285076fcb907fe4230991f73558330874e
samuelyo <1120780416@qq.com>
2017-09-22 14:11:06 +0800

    Leetcode 552 DP and int[n+1][2][3]dp and let dp[i][j][k] denote the number of valid sequences of length i where there can be at most j A in the entire sequence and there can be at most k trailing L and int val = dp[i-1][j][2] and val += dp[i-1][j-1][2] and val += dp[i-1][j][k-1] and dp[i][j][k] = val

---
+++ 552StudentAttendanceRecordII.java
@@ -0,0 +1,47 @@
+package com.leetcode.StudentAttendanceRecordII;
+
+public class StudentAttendanceRecordII {
+
+	public static void main(String[] args) {
+
+		int n = 2;
+		System.out.println(checkRecord(n));
+
+	}
+
+	public static int M = 1000000007;
+
+	// Let f[i][j][k] denote the # of valid sequences of length i where:
+	// There can be at most j A's in the entire sequence.
+	// There can be at most k trailing L's.
+	public static int checkRecord(int n) {
+
+		if (n == 0) {
+			return 0;
+		}
+		int[][][] dp = new int[n + 1][2][3];
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+				dp[0][i][j] = 1;
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < 2; j++) {
+				for (int k = 0; k < 3; k++) {
+					int val = dp[i - 1][j][2];
+					if (j > 0) {
+						val = (val + dp[i - 1][j - 1][2]) % M;
+					}
+					if (k > 0) {
+						val = (val + dp[i - 1][j][k - 1]) % M;
+					}
+					dp[i][j][k] = val;
+				}
+			}
+		}
+		return dp[n][1][2];
+
+	}
+
+}
