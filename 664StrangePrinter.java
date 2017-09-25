c945c16ba199e7f2cb966b8c9a0860090c723c47
samuelyo <1120780416@qq.com>
2017-09-25 09:43:51 +0800

    Leetcode 664 DP and from top tp bottom and for (int k = j + 1; k <= j + i; k++) and int temp = dp[j][k - 1] + dp[k][j + i] and if (s.charAt(k - 1) == s.charAt(j + i)) and temp -- and dp[j][j + i] = Math.min(dp[j][j + i], temp) and return dp[0][n-1]

---
+++ 664StrangePrinter.java
@@ -0,0 +1,35 @@
+package com.leetcode.StrangePrinter;
+
+public class StrangePrinter {
+
+	public static void main(String[] args) {
+
+		String s = "aaabbb";
+		System.out.println(strangePrinter(s));
+
+	}
+
+	public static int strangePrinter(String s) {
+
+		int n = s.length();
+		if (n == 0)
+			return 0;
+
+		int[][] dp = new int[101][101];
+		for (int i = 0; i < n; i++)
+			dp[i][i] = 1;
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < n - i; j++) {
+				dp[j][j + i] = i + 1;
+				for (int k = j + 1; k <= j + i; k++) {
+					int temp = dp[j][k - 1] + dp[k][j + i];
+					if (s.charAt(k - 1) == s.charAt(j + i))
+						temp--;
+					dp[j][j + i] = Math.min(dp[j][j + i], temp);
+				}
+			}
+		}
+		return dp[0][n - 1];
+	}
+}
