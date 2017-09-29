a38f3ed9caccd09eb9031c979503c0e81661562c
samuelyo <1120780416@qq.com>
2017-09-29 09:19:48 +0800

    Leetcode 87 DP and dp[i][j][1] = (s1.charAt(i) == s2.charAt(j)) and dp[i][j][k] && dp[i + k][j + k][len - k]) || (dp[i + k][j][len - k] && dp[i][j + len - k][k] then dp[i][j][len] = true

---
+++ 87ScrambleString2.java
@@ -0,0 +1,43 @@
+package com.leetcode.ScrambleString;
+
+public class ScrambleString2 {
+
+	public static void main(String[] args) {
+		
+		String s1 = "abc";
+		String s2 = "bca";
+		System.out.println(isScramble(s1, s2));
+
+	}
+	
+	public static boolean isScramble(String s1, String s2) {
+		
+		if(s1.length() != s2.length()) {
+			return false;
+		}
+		if(s1.equals(s2)) {
+			return true;
+		}
+		int n = s1.length();
+		boolean[][][] dp = new boolean[n][n][n+1];
+		for(int i = 0;i<n;i++) {
+			for(int j = 0;j<n;j++) {
+				dp[i][j][1] = (s1.charAt(i) == s2.charAt(j));
+			}
+		}
+		for(int len = 2;len<=n;len++) {
+			for(int i = 0;i<=n-len;i++) {
+				for(int j = 0;j<=n-len;j++) {
+					for(int k = 1;k<len;k++) {
+						if((dp[i][j][k] && dp[i + k][j + k][len - k]) || (dp[i + k][j][len - k] && dp[i][j + len - k][k])) {
+							dp[i][j][len] = true;
+						}
+					}
+				}
+			}
+		}
+		return dp[0][0][n];
+		
+	}
+
+}
