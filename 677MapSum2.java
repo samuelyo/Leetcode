64ba29979cc5cba6d1454faeb8bbbdf48381fcca
samuelyo <1120780416@qq.com>
2017-10-02 10:09:53 +0800

    Leetcode 677 Map<String, Integer> map and Map<String, Integer> original and firstly calculate the diff to be added to prefixes and creating all prefixes and update/insert all prefixes with new value

---
+++ 677MapSum2.java
@@ -0,0 +1,34 @@
+package com.leetcode.MapSumPairs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSum2 {
+
+	Map<String, Integer> map;
+	Map<String, Integer> original;
+
+	public MapSum2() {
+	    map = new HashMap<>();
+	    original = new HashMap<>();
+	}
+
+	public void insert(String key, int val) {
+		
+		val -= original.getOrDefault(key, 0); // calculate the diff to be added
+												// to prefixes
+		String s = "";
+		for (char c : key.toCharArray()) {
+			s += c; // creating all prefixes
+			map.put(s, map.getOrDefault(s, 0) + val); // update/insert all
+														// prefixes with new
+														// value
+		}
+		original.put(key, original.getOrDefault(key, 0) + val);
+	}
+
+	public int sum(String prefix) {
+		return map.getOrDefault(prefix, 0);
+	}
+
+}
