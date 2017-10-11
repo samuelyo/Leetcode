9ada8956cf1f0e135b1dc2f21fe78467b4c7520a
samuelyo <1120780416@qq.com>
2017-10-11 13:42:57 +0800

    Leetcode 10 Recurison and if(p.length == 0) and if(p.length == 1) and if(p.length > 1 and p.charAt(1) == *) and else

---
+++ 10RegularExpressionMatching2.java
@@ -0,0 +1,29 @@
+package com.leetcode.RegularExpressionMatching;
+
+public class RegularExpressionMatching2 {
+
+	public static void main(String[] args) {
+		
+		String s = "aab";
+		String p = "c*a*b";
+		System.out.println(isMatch(s, p));
+		
+	}
+
+	public static boolean isMatch(String s, String p) {
+
+		if(p.length() == 0) {
+			return s.length() == 0;
+		}
+//		if(p.length() == 1) {
+//			return s.length() == 1 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+//		}
+		if(p.length() > 1 && p.charAt(1) == '*') {
+			return isMatch(s, p.substring(2)) || (s.length() != 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p));
+		}else{
+			return s.length() != 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p.substring(1));
+		}
+		
+	}
+
+}
