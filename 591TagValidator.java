b1acb41821cc0e4ee7e82ea69382cb0b3ab0cede
samuelyo <1120780416@qq.com>
2017-10-20 11:25:40 +0800

    Leetcode 591 Stack<String> stack = new Stack<String>() and if (code.startsWith(<CDATA, i)) and else if (code.startsWith(</, i)) and else if (code.startsWith(<, i)) and else i++

---
+++ 591TagValidator.java
@@ -0,0 +1,56 @@
+package com.leetcode.TagValidator;
+
+import java.util.Stack;
+
+public class TagValidator {
+
+	public static void main(String[] args) {
+		
+		String code = "<DIV>This is the first line <![CDATA[<div>]]></DIV>";
+		System.out.println(isValid(code));
+
+	}
+
+	public static boolean isValid(String code) {
+
+		Stack<String> stack = new Stack<String>();
+		for (int i = 0; i < code.length();) {
+			if (i > 0 && stack.isEmpty())
+				return false;
+			if (code.startsWith("<![CDATA[", i)) {
+				int j = i + 9;
+				i = code.indexOf("]]>", j);
+				if (i < 0)
+					return false;
+				i += 3;
+			} else if (code.startsWith("</", i)) {
+				int j = i + 2;
+				i = code.indexOf('>', j);
+				if (i < 0 || i == j || i - j > 9)
+					return false;
+				for (int k = j; k < i; k++) {
+					if (!Character.isUpperCase(code.charAt(k)))
+						return false;
+				}
+				String s = code.substring(j, i++);
+				if (stack.isEmpty() || !stack.pop().equals(s))
+					return false;
+			} else if (code.startsWith("<", i)) {
+				int j = i + 1;
+				i = code.indexOf('>', j);
+				if (i < 0 || i == j || i - j > 9)
+					return false;
+				for (int k = j; k < i; k++) {
+					if (!Character.isUpperCase(code.charAt(k)))
+						return false;
+				}
+				String s = code.substring(j, i++);
+				stack.push(s);
+			} else {
+				i++;
+			}
+		}
+		return stack.isEmpty();
+	}
+
+}
