55310f8893e78eb45f9dd9fa5f424917d320eb46
samuelyo <1120780416@qq.com>
2017-11-10 13:18:46 +0800

    Leetcode 204 boolean[] notPrime = new boolean[n] and count = 0 and for(int i = 2;i<n;i++) if(notPrime[i] == false) count ++ and if(i*i < n) and for(int j = 2;i*j < n j++) notPrime[i*j] = true

---
+++ 204CountPrimes3.java
@@ -0,0 +1,30 @@
+package com.leetcode.CountPrimes;
+
+public class CountPrimes3 {
+
+	public static void main(String[] args) {
+
+		int n = 100;
+		System.out.println(countPrimes(n));
+
+	}
+
+	public static int countPrimes(int n) {
+
+		boolean[] notPrime = new boolean[n];
+		int count = 0;
+		for (int i = 2; i < n; i++) {
+			if (notPrime[i] == false) {
+				count++;
+				if(i*i < n){ //check up to Math.sqrt(n)
+					for (int j = 2; i * j < n; j++) {
+						notPrime[i * j] = true;
+					}
+				}
+			}
+		}
+
+		return count;
+	}
+
+}
