d23805c11b292ea63fb6c41c42d7e5a80bc697ae
samuelyo <1120780416@qq.com>
2017-11-18 12:25:30 +0800

    Leetcode 292 return not(num % 4 == 0)

---
+++ 292NimGame2.java
@@ -0,0 +1,17 @@
+package com.leetcode.NimGame;
+
+public class NimGame2 {
+
+	public static void main(String[] args) {
+		
+		int n = 4;
+		System.out.println(canWinNim(n));
+	}
+	
+	public static boolean canWinNim(int n) {
+		
+		return (n % 4 != 0);
+		
+	}
+
+}
