e8a8adfda48981fa3b1f0db63b3b1de9ecb5a18c
samuelyo <1120780416@qq.com>
2017-12-29 12:56:32 +0800

    Leetcode 31 if(i == 0) else int cha = nums[i - 1] and j = nums.length - 1 and while(nums[j] <= cha) j-- and nums[i - 1] = nums[j] nums[j] = cha nums[j] = cha and reverse(lefy = i and right = nums.length - 1)

---
+++ 31NextPermutation4.java
@@ -0,0 +1,50 @@
+package com.leetcode.NextPermutation;
+
+import java.util.Arrays;
+
+public class NextPermutation4 {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 5, 1, 1 };
+		nextPermutation(nums);
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ");
+		}
+
+	}
+
+	public static void nextPermutation(int[] nums) {
+
+		if (nums == null || nums.length == 0 || nums.length == 1) {
+			return;
+		}
+		int i = nums.length - 1;
+		while (i > 0 && nums[i] <= nums[i - 1]) {
+			i--;
+		}
+		if (i == 0) {
+			Arrays.sort(nums);
+			return;
+		} else {
+			int cha = nums[i - 1];
+			int j = nums.length - 1;
+			while (nums[j] <= cha) {
+				j--;
+			}
+			nums[i - 1] = nums[j];
+			nums[j] = cha;
+			int left = i;
+			int right = nums.length - 1;
+			while (left < right) {
+				int temp = nums[left];
+				nums[left] = nums[right];
+				nums[right] = temp;
+				left++;
+				right--;
+			}
+		}
+
+	}
+
+}
