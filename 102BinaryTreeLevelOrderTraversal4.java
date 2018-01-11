f69c341ba688f05747238995bc8f917cdc2c8445
samuelyo <1120780416@qq.com>
2018-01-11 13:54:02 +0800

    Leetcode 102 Queue<TreeNode> queue = new LinkedList<TreeNode>() and queue.add(root) while(not queue.isEmpty()) int size = queue.size() for(int i = 0;i<size;i++) TreeNode cur = queue.poll() if(cur.left not null) queue.add(cur.left) if(cur.right not null) queue.add(cur.right)

---
+++ 102BinaryTreeLevelOrderTraversal4.java
@@ -0,0 +1,60 @@
+package com.leetcode.BinaryTreeLevelOrderTraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal4 {
+
+	public static void main(String[] args) {
+	
+		TreeNode root = new TreeNode(3);
+		TreeNode node1 = new TreeNode(9);
+		TreeNode node2 = new TreeNode(20);
+		TreeNode node3 = new TreeNode(15);
+		TreeNode node4 = new TreeNode(7);
+		
+		root.left = node1;
+		root.right = node2;
+		node2.left = node3;
+		node2.right = node4;
+		
+		List<List<Integer>> res = levelOrder(root);
+		for(int i = 0;i<res.size();i++){
+			List<Integer> cur = res.get(i);
+			for(int j = 0;j<cur.size();j++){
+				System.out.print(cur.get(j) + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+	public static List<List<Integer>> levelOrder(TreeNode root) {
+		
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if(root == null) {
+			return res;
+		}
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			List<Integer> list = new ArrayList<Integer>();
+			for(int i = 0;i<size;i++) {
+				TreeNode cur = queue.poll();
+				list.add(cur.val);
+				if(cur.left != null) {
+					queue.add(cur.left);
+				}
+				if(cur.right != null) {
+					queue.add(cur.right);
+				}
+			}
+			res.add(list);
+		}
+		return res;
+		
+	}
+}
