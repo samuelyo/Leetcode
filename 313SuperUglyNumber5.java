dcc853c1adbf9564c27eac92cc9c326bc53003ce
samuelyo <1120780416@qq.com>
2018-02-07 13:01:14 +0800

    Leetcode 313 private static class Num implements Comparable<Num> int val, int idx, int p public Num(int val, int idx, int p) {this.val = val,this.idx = idx, this.p = p} public int compareTo(Num that) return this.val - that.val and int[] ugly = new int[n] PriorityQueue<Num> pq = new PriorityQueue<Num>() for(int i = 0;i<primes.length;i++) pq.add(new Num(primes[i], 1, primes[i])) ugly[0] = 1 for(int i = 1;i<n;i++) ugly[i] = pq.peek().val while(pq.peek().val == ugly[i]) Num nxt = pq.poll() pq.add(new Num(nxt.p * ufly[nxt.idx], nxt.idx + 1, nxt.p)) return ugly[n - 1]

---
+++ 313SuperUglyNumber5.java
@@ -0,0 +1,53 @@
+package com.leetcode.SuperUglyNumber;
+
+import java.util.PriorityQueue;
+
+public class SuperUglyNumber5 {
+
+	private static class Num implements Comparable<Num> {
+		int val;
+		int idx;
+		int p;
+
+		public Num(int val, int idx, int p) {
+			this.val = val;
+			this.idx = idx;
+			this.p = p;
+		}
+
+		@Override
+		public int compareTo(Num that) {
+			return this.val - that.val;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int n = 12;
+		int[] primes = { 2, 7, 13, 19 };
+		System.out.println(nthSuperUglyNumber(n, primes));
+
+	}
+
+	public static int nthSuperUglyNumber(int n, int[] primes) {
+
+		int[] ugly = new int[n];
+
+		PriorityQueue<Num> pq = new PriorityQueue<>();
+		for (int i = 0; i < primes.length; i++)
+			pq.add(new Num(primes[i], 1, primes[i]));
+		ugly[0] = 1;
+
+		for (int i = 1; i < n; i++) {
+			ugly[i] = pq.peek().val;
+			while (pq.peek().val == ugly[i]) {
+				Num nxt = pq.poll();
+				pq.add(new Num(nxt.p * ugly[nxt.idx], nxt.idx + 1, nxt.p));
+			}
+		}
+
+		return ugly[n - 1];
+
+	}
+
+}
