56547c0f25f8e9e1d2a0cbf807254e1ae57660ac
samuelyo <1120780416@qq.com>
2018-02-07 13:45:04 +0800

    Leetcode 319 return (int)Math.sqrt(n)

---
+++ 319BulbSwitcher2.java
@@ -0,0 +1,18 @@
+package com.leetcode.BulbSwitcher;
+
+public class BulbSwitcher2 {
+
+	public static void main(String[] args) {
+
+		int n = 99;
+		System.out.println(bulbSwitch(n));
+
+	}
+
+	public static int bulbSwitch(int n) {
+		
+		return (int)Math.sqrt(n);
+		
+	}
+
+}
