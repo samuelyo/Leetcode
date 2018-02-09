cf6bb00026ab00f5a9ee951e65c61c806e28d0b0
samuelyo <1120780416@qq.com>
2018-02-09 14:56:00 +0800

    Leetcode 331 String[] nodes = preorder.split(,) int diff = 1 for(String node : nodes) diff --  if(diff < 0) return false if(node not equals(#)) diff += 2 return diff == 0

---
+++ 331VerifyPreorderSerializationOfABinaryTree5.java
@@ -0,0 +1,28 @@
+package com.leetcode.VerifyPreorderSerializationOfABinaryTree;
+
+public class VerifyPreorderSerializationOfABinaryTree5 {
+
+	public static void main(String[] args) {
+
+		String preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#";
+		System.out.println(isValidSerialization(preorder));
+
+	}
+
+	public static boolean isValidSerialization(String preorder) {
+
+		String[] nodes = preorder.split(",");
+		int diff = 1;
+		for (String node : nodes) {
+			diff--;
+			if (diff < 0) {
+				return false;
+			}
+			if (!node.equals("#")) {
+				diff += 2;
+			}
+		}
+		return diff == 0;
+	}
+
+}
