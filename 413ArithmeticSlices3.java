735c7f61dfad85665c3fe7f8cfd599d7bf125407
samuelyo <1120780416@qq.com>
2018-02-26 16:39:01 +0800

    Leetcode 413 int res = 0 Integer[][] dp = new Integer[A.length][A.length] for(int i = 0;i<A.length - 2;i++) if(A[i + 1] * 2 == A[i] + A[i + 2]) dp[i][i + 2] = A[i + 1] - A[i] res ++ for(int i = 0;i<A.length - 2;i++) for(int j = i + 2;j<A.length;j++) if(dp[i][j - 1] not null && dp[i][j - 1] == A[j] - A[j - 1]) res ++ dp[i][j] = dp[i][j - 1] return res

---
+++ 413ArithmeticSlices3.java
@@ -0,0 +1,34 @@
+package com.leetcode.ArithmeticSlices;
+
+public class ArithmeticSlices3 {
+
+	public static void main(String[] args) {
+		
+		int[] A = {1,2,3,4,5};
+		System.out.println(numberOfArithmeticSlices(A));
+
+	}
+	
+	public static int numberOfArithmeticSlices(int[] A) {
+		
+		int res = 0;
+		Integer[][] dp = new Integer[A.length][A.length];
+		for(int i = 0;i<A.length - 2;i++) {
+			if(A[i + 1] * 2 == A[i] + A[i + 2]) {
+				dp[i][i + 2] = A[i + 1] - A[i];
+				res ++;
+			}
+		}
+		for(int i = 0;i<A.length - 2;i++) {
+			for(int j = i + 2;j<A.length;j++) {
+				if(dp[i][j - 1] != null && dp[i][j - 1] == A[j] - A[j - 1]) {
+					res ++;
+					dp[i][j] = dp[i][j - 1];
+				}
+			}
+		}
+		return res;
+		
+	}
+
+}
