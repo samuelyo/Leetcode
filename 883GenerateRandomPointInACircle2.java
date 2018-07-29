d2faf8507f5f74ce2f68a73bb0665925f8c79323
samuelyo <1120780416@qq.com>
2018-07-29 10:21:07 +0800

    Leetcode 883

---
+++ 883GenerateRandomPointInACircle2.java
@@ -0,0 +1,17 @@
+package com.leetcode.GenerateRandomPointInACircle;
+
+public class GenerateRandomPointInACircle {
+    double radius, x, y;
+
+    public GenerateRandomPointInACircle(double radius, double x_center, double y_center) {
+        this.radius = radius;
+        x = x_center;
+        y = y_center;
+    }
+
+    public double[] randPoint() {
+        double len = Math.sqrt(Math.random()) * radius;
+        double deg = Math.random() * 2 * Math.PI;
+        return new double[]{x + len * Math.cos(deg), y + len * Math.sin(deg)};
+    }
+}
