2f6c9fcb545401f80b58c38f908755d3d4b29ddd
samuelyo <1120780416@qq.com>
2018-08-05 15:48:26 +0800

    Leetcode 884

---
+++ 884DecodedStringAtIndex4.java
@@ -0,0 +1,31 @@
+package com.leetcode.DecodedStringAtIndex;
+
+public class DecodedStringAtIndex4 {
+    public static void main(String[] args) {
+        String S = "a2345678999999999999999";
+        int K = 1;
+        String res = decodeAtIndex(S, K);
+        System.out.println(res);
+    }
+
+    public static String decodeAtIndex(String S, int K) {
+        long N = 0L;
+        int i;
+        char[] chs = S.toCharArray();
+        for (i = 0; N < K; i++) {
+            N = Character.isDigit(chs[i]) ? N * (chs[i] - '0') : N + 1;
+        }
+        i--;
+        while (true) {
+            if (Character.isDigit(chs[i])) {
+                N /= chs[i] - '0';
+                K %= N;
+            } else if (K == 0 || K == N) {
+                return "" + chs[i];
+            } else {
+                N--;
+            }
+            i--;
+        }
+    }
+}
