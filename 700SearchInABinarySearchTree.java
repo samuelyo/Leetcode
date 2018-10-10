c3d3f03e7cfd3bba1fbab7a97738ff4e800d4d75
samuelyo <1120780416@qq.com>
2018-10-10 16:45:55 +0800

    Leetcode 700

---
+++ 700SearchInABinarySearchTree.java
@@ -0,0 +1,20 @@
+package com.leetcode.SearchInABinarySearchTree;
+
+public class SearchInABinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
