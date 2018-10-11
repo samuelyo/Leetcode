978a8d3a3d8b410d8771a284ceb9648b5923f5bf
samuelyo <1120780416@qq.com>
2018-10-11 16:49:53 +0800

    Leetcode 704

---
+++ 704BinarySearch.java
@@ -0,0 +1,28 @@
+package com.leetcode.BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
