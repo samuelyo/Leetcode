b3226d9070aaddb46986a1e91beae389fd761702
samuelyo <1120780416@qq.com>
2018-10-16 11:43:17 +0800

    Leetcode 923

---
+++ 923ThreeSumWithMultiplicity.java
@@ -0,0 +1,27 @@
+package com.leetcode.ThreeSumWithMultiplicity;
+
+public class ThreeSumWithMultiplicity {
+    public static void main(String[] args) {
+        int[] A = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+        int target = 8;
+        System.out.println(threeSumMulti(A, target));
+    }
+
+    public static int threeSumMulti(int[] A, int target) {
+        long[] c = new long[101];
+        for (int a : A) c[a]++;
+        long res = 0;
+        for (int i = 0; i <= 100; i++)
+            for (int j = i; j <= 100; j++) {
+                int k = target - i - j;
+                if (k > 100 || k < 0) continue;
+                if (i == j && j == k)
+                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
+                else if (i == j && j != k)
+                    res += c[i] * (c[i] - 1) / 2 * c[k];
+                else if (j < k)
+                    res += c[i] * c[j] * c[k];
+            }
+        return (int) (res % (1e9 + 7));
+    }
+}
