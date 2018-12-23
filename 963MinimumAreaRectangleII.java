22a3926b6a99457e9cc96de58498819516660f14
samuelyo <1120780416@qq.com>
2018-12-23 13:24:45 +0800

    Leetcode 963

---
+++ 963MinimumAreaRectangleII.java
@@ -0,0 +1,66 @@
+package com.leetcode.MinimumAreaRectangleII;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MinimumAreaRectangleII {
+    public static void main(String[] args) {
+        int[][] points = {{3, 1}, {1, 1}, {0, 1}, {2, 1}, {3, 3}, {3, 2}, {0, 2}, {2, 3}};
+        double res = minAreaFreeRect(points);
+        System.out.println(res);
+    }
+
+    public static double minAreaFreeRect(int[][] points) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] p : points) {
+            map.putIfAbsent(p[0], new HashSet<>());
+            map.get(p[0]).add(p[1]);
+        }
+        double min = Double.MAX_VALUE;
+        int n = points.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int[] p1 = points[i], p2 = points[j], p3 = points[k];
+                    if (ver(p1, p2, p3)) {
+                        int[] temp = p4(p1, p2, p3);
+                        if (map.containsKey(temp[0]) && map.get(temp[0]).contains(temp[1])) {
+                            min = Math.min(min, area(p1, p2, p3));
+                        }
+                    }
+                    if (ver(p2, p1, p3)) {
+                        int[] temp1 = p4(p2, p1, p3);
+                        if (map.containsKey(temp1[0]) && map.get(temp1[0]).contains(temp1[1])) {
+                            min = Math.min(min, area(p2, p1, p3));
+                        }
+                    }
+                    if (ver(p3, p2, p1)) {
+                        int[] temp2 = p4(p3, p2, p1);
+                        if (map.containsKey(temp2[0]) && map.get(temp2[0]).contains(temp2[1])) {
+                            min = Math.min(min, area(p3, p2, p1));
+                        }
+                    }
+                }
+            }
+        }
+        return min == Double.MAX_VALUE ? 0 : min;
+    }
+
+    public static int[] p4(int[] p1, int[] p2, int[] p3) { // find point p4
+        return new int[]{p3[0] - p1[0] + p2[0], p3[1] - p1[1] + p2[1]};
+    }
+
+    public static boolean ver(int[] p1, int[] p2, int[] p3) {//detemine vector p1p2 and vector p1p3 are vertical or not
+        int x1 = p2[0] - p1[0], y1 = p2[1] - p1[1];
+        int x2 = p3[0] - p1[0], y2 = p3[1] - p1[1];
+        return x1 * x2 + y1 * y2 == 0;
+    }
+
+    public static double area(int[] p1, int[] p2, int[] p3) {
+        int x1 = p2[0] - p1[0], y1 = p2[1] - p1[1];
+        int x2 = p3[0] - p1[0], y2 = p3[1] - p1[1];
+        return Math.sqrt(x1 * x1 + y1 * y1) * Math.sqrt(x2 * x2 + y2 * y2);
+    }
+}
