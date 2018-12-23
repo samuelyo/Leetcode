c936fcd68c18af116f1aed84fce6cc67b8ed69ad
samuelyo <1120780416@qq.com>
2018-12-23 14:02:51 +0800

    Leetcode 964

---
+++ 964LeastOperatorsToExpressNumber.java
@@ -0,0 +1,26 @@
+package com.leetcode.LeastOperatorsToExpressNumber;
+
+public class LeastOperatorsToExpressNumber {
+    public static void main(String[] args) {
+        System.out.println(leastOpsExpressTarget(100, 100000000));
+    }
+
+    public static int leastOpsExpressTarget(int x, int target) {
+        int pos = 0, neg = 0, k = 0, pos2, neg2, cur;
+        while (target > 0) {
+            cur = target % x;
+            target /= x;
+            if (k > 0) {
+                pos2 = Math.min(cur * k + pos, (cur + 1) * k + neg);
+                neg2 = Math.min((x - cur) * k + pos, (x - cur - 1) * k + neg);
+                pos = pos2;
+                neg = neg2;
+            } else {
+                pos = cur * 2;
+                neg = (x - cur) * 2;
+            }
+            k++;
+        }
+        return Math.min(pos, k + neg) - 1;
+    }
+}
