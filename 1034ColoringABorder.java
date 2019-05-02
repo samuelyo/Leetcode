1e312cfc090640227bcd20ad39dac3bbed24c6f3
samuelyo <1120780416@qq.com>
2019-05-02 13:41:33 +0800

    Leetcode 1034

---
+++ 1034ColoringABorder.java
@@ -0,0 +1,45 @@
+package com.leetcode.ColoringABorder;
+
+import java.util.Arrays;
+
+public class ColoringABorder {
+    public static void main(String[] args) {
+        int[][] grid = {{1, 1}, {1, 2}};
+        int[][] res = colorBorder(grid, 0, 0, 3);
+        for (int i = 0; i < res.length; i++) {
+            Arrays.toString(res[i]);
+        }
+
+    }
+
+    public static int[][] colorBorder(int[][] grid, int r0, int c0, int color) {
+        if (grid == null || grid.length == 0)
+            return null;
+        int oldColor = grid[r0][c0];
+        dfs(grid, r0, c0, oldColor);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] < 0)
+                    grid[i][j] = color;
+            }
+        }
+        return grid;
+    }
+
+    public static void dfs(int[][] grid, int i, int j, int oldColor) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != oldColor)
+            return;
+        grid[i][j] = -oldColor;
+        dfs(grid, i + 1, j, oldColor);
+        dfs(grid, i - 1, j, oldColor);
+        dfs(grid, i, j + 1, oldColor);
+        dfs(grid, i, j - 1, oldColor);
+        if (i > 0 && j > 0 && i < grid.length - 1 && j < grid[0].length - 1
+                && oldColor == Math.abs(grid[i + 1][j])
+                && oldColor == Math.abs(grid[i - 1][j])
+                && oldColor == Math.abs(grid[i][j + 1])
+                && oldColor == Math.abs(grid[i][j - 1]))
+            grid[i][j] = oldColor;
+    }
+}
+
