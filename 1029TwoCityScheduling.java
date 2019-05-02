65f969367cbf60a6768846ddcec3544581caf44d
samuelyo <1120780416@qq.com>
2019-05-02 13:23:13 +0800

    Leetcode 1029

---
+++ 1029TwoCityScheduling.java
@@ -0,0 +1,26 @@
+package com.leetcode.TwoCityScheduling;
+
+public class TwoCityScheduling {
+    public static void main(String[] args) {
+        int[][] costs = {{10, 20}, {30, 200}, {400, 50}, {30, 20}};
+        int res = twoCitySchedCost(costs);
+        System.out.println(res);
+    }
+
+    public static int twoCitySchedCost(int[][] costs) {
+        int N = costs.length / 2;
+        int[][] dp = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = dp[i - 1][0] + costs[i - 1][0];
+        }
+        for (int j = 1; j <= N; j++) {
+            dp[0][j] = dp[0][j - 1] + costs[j - 1][1];
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j] + costs[i + j - 1][0], dp[i][j - 1] + costs[i + j - 1][1]);
+            }
+        }
+        return dp[N][N];
+    }
+}
