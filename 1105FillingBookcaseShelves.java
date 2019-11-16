c48d1c9ccb347e9915740113c8b0dce8806280ca
samuelyo <1120780416@qq.com>
2019-11-16 12:00:08 +0800

    Leetcode 1105

---
+++ 1105FillingBookcaseShelves.java
@@ -0,0 +1,26 @@
+package com.leetcode.FillingBookcaseShelves;
+
+public class FillingBookcaseShelves {
+    public static void main(String[] args) {
+        int[][] books = {{1, 1}, {2, 3}, {2, 3}, {1, 1}, {1, 1}, {1, 1}, {1, 2}};
+        int shelf_width = 4;
+        int res = minHeightShelves(books, shelf_width);
+        System.out.println(res);
+    }
+
+    public static int minHeightShelves(int[][] books, int shelf_width) {
+        int[] dp = new int[books.length + 1];
+
+        for (int i = 1; i <= books.length; i++) {
+            int width = books[i - 1][0];
+            int height = books[i - 1][1];
+            dp[i] = dp[i - 1] + height;
+            for (int j = i - 1; j > 0 && width + books[j - 1][0] <= shelf_width; j--) {
+                height = Math.max(height, books[j - 1][1]);
+                width += books[j - 1][0];
+                dp[i] = Math.min(dp[i], dp[j - 1] + height);
+            }
+        }
+        return dp[books.length];
+    }
+}
