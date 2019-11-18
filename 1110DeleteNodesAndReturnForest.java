af23d74b22b3820f8714b89a5879ac949ef8c9b9
samuelyo <1120780416@qq.com>
2019-11-19 00:37:23 +0800

    Leetcode 1110

---
+++ 1110DeleteNodesAndReturnForest.java
@@ -0,0 +1,33 @@
+package com.leetcode.DeleteNodesAndReturnForest;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DeleteNodesAndReturnForest {
+    public static void main(String[] args) {
+
+    }
+
+    public static Set<Integer> to_delete_set;
+    public static List<TreeNode> res;
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        to_delete_set = new HashSet<>();
+        res = new ArrayList<>();
+        for (int i : to_delete)
+            to_delete_set.add(i);
+        helper(root, true);
+        return res;
+    }
+
+    private TreeNode helper(TreeNode node, boolean is_root) {
+        if (node == null) return null;
+        boolean deleted = to_delete_set.contains(node.val);
+        if (is_root && !deleted) res.add(node);
+        node.left = helper(node.left, deleted);
+        node.right = helper(node.right, deleted);
+        return deleted ? null : node;
+    }
+}
